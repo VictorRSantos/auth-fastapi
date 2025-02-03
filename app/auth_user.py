51755188bc5467f7c1c143ebfa3381ee8c251c57
Victor R Santos <vrsisa@gmail.com>
2025-02-03 10:41:28 -0300

    Adicionando user login

--- app/auth_user.py
+++ app/auth_user.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from fastapi import status
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
@@ -5,6 +6,11 @@ from sqlalchemy.exc import IntegrityError
 from app.db.models import UserModel
 from app.schemas import User
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+ALGORITHM = config('ALGORITHM')
 
 crypt_context = CryptContext(schemes=['sha256_crypt'])
 
@@ -25,3 +31,34 @@ class UserUseCases:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail='User already exists'
             )
+    
+    def user_login(self, user: User, expires_in: int = 30):
+        user_on_db = self.db_session.query(UserModel).filter_by(username=user.username).first()
+
+        if user_on_db is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Invalid username or password'
+            )
+        
+        if not crypt_context.verify(user.password, user_on_db.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Invalid username or password'
+            )
+        
+        exp = datetime.utcnow() + timedelta(minutes=expires_in)
+
+        payload = {
+            'sub': user.username,
+            'exp': exp
+        }
+
+        acess_token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+        return {
+                'acess_token':acess_token,
+                'exp':exp.isoformat()
+        }
+
+
